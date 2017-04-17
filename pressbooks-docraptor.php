784b4b526f3a300975a8fc5d27547ddcd74b9c8c
Ned Zimmerman <ned@bight.ca>
2017-04-17 12:25:49 -0300

    Network-only plugin (fix #10).

--- pressbooks-docraptor.php
+++ pressbooks-docraptor.php
@@ -3,12 +3,13 @@
 Plugin Name: Docraptor for Pressbooks
 Plugin URI: https://pressbooks.org
 Description: Docraptor exporter for Pressbooks.
-Version: 1.1.0
+Version: 1.2.0-dev
 Author: Pressbooks (Book Oven Inc.)
 Author URI: https://pressbooks.org
 Text Domain: pressbooks-docraptor
 License: GPLv2
 GitHub Plugin URI: https://github.com/pressbooks/pressbooks-docraptor
+Network: true
 Release Asset: true
 */
 
